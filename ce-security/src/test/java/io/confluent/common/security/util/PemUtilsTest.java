3d53c5587cb85b247a56bdb2637978af77e3ea6d
Rajini Sivaram <rajinisivaram@googlemail.com>
2018-12-10 22:29:38 +0000

    CONFLUENT: add utility method to write public keys to pem files (#79)
    This was originally committed by Xavier to private-common, reviewed by Andy Coates:
    https://github.com/confluentinc/private-common/commit/3e55e0516900a69ee8b5a3d24a2f7d220cd7e311
    
    Reviewers: Stanislav Kozlovski <stanislav_kozlovski@outlook.com>, Ismael Juma <ismael@juma.me.uk>, Xavier Léauté <xl+github@xvrl.net>

--- ce-security/src/test/java/io/confluent/common/security/util/PemUtilsTest.java
+++ ce-security/src/test/java/io/confluent/common/security/util/PemUtilsTest.java
@@ -6,18 +6,35 @@ package io.confluent.common.security.util;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PemUtilsTest {
 
   @Test
-  public void loadPublicKey() throws Exception {
+  public void testLoadPublicKey() throws Exception {
     InputStream resourceAsStream = this.getClass().getResourceAsStream("/publickey.pem");
     PublicKey publicKey = PemUtils.loadPublicKey(resourceAsStream);
     assertEquals("RSA", publicKey.getAlgorithm());
     assertEquals("X.509", publicKey.getFormat());
+  }
+
+  @Test
+  public void testWritePublicKey() throws Exception {
+    final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+    generator.initialize(2048);
+    KeyPair keyPair = generator.generateKeyPair();
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    PemUtils.writePublicKey(out, keyPair.getPublic());
+    String outputString = out.toString("utf8");
+    assertTrue(outputString.startsWith("-----BEGIN PUBLIC KEY-----\n"));
+    assertTrue(outputString.endsWith("-----END PUBLIC KEY-----\n"));
   }
 }
