bb94ec04e8fea99f2e7ee40eb56af230a84dcd58
Gwen (Chen) Shapira <cshapi@gmail.com>
2019-01-03 20:38:14 -0800

    CONFLUENT: minor license fix (#106)

--- ce-broker-plugins/src/main/java/io/confluent/kafka/server/plugins/auth/PlainSaslServer.java
+++ ce-broker-plugins/src/main/java/io/confluent/kafka/server/plugins/auth/PlainSaslServer.java
@@ -1,27 +1,8 @@
+// (Copyright) [2019 - 2019] Confluent, Inc.
 package io.confluent.kafka.server.plugins.auth;
 
 import io.confluent.kafka.multitenant.MultiTenantPrincipal;
 import io.confluent.kafka.multitenant.MultiTenantSaslServer;
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * rhoover - split out credential check, added stats and logging
- */
-
 import io.confluent.kafka.multitenant.TenantMetadata;
 import io.confluent.kafka.server.plugins.auth.stats.AuthenticationStats;
 import io.confluent.kafka.server.plugins.auth.stats.TenantAuthenticationStats;
