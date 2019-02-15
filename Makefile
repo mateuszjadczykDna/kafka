mateuszjadczykDna/kafka

commit f6e5c51c2522e061bc523cddbd2795c0bdd9129c

Build cc-services (soak-clients, ce-trogdor)  docker containers and helm charts from make (#151)
soak-clients and ce-trogdor now build from the build-docker and push-docker make targets. This should make our Semaphore CI push a new docker build of them alongside ce-kafka.

Add new make targets:
* helm-deploy-soak - deploy either ce-trogdor or soak-clients to the kubernetes cluster your kubectl is configured to target
* build-docker-cc-services - build both ce-trogdor and the soak-clients images
* push-docker-cc-services - push the latest built images of the ce-trogdor and soak-clients images to artifactory


This commit also syncs the mk-include changes[1] via git subtree pull
[1] https://github.com/confluentinc/cc-mk-include/pull/111

Author: Hector <hector@confluent.io>
Reviewers: Stanislav<stanislav@confluent.io>

Makefile
@@ -5,7 +5,48 @@ MASTER_BRANCH := ce-trunk
 KAFKA_VERSION := $(shell awk 'sub(/.*version=/,""){print $1}' ./gradle.properties)
 VERSION_POST := -$(KAFKA_VERSION)
 
+# Override docker default targets
+BUILD_DOCKER_OVERRIDE := build-docker-override
+PUSH_DOCKER_OVERRIDE := push-docker-override
+
 include ./mk-include/cc-begin.mk
 include ./mk-include/cc-semver.mk
 include ./mk-include/cc-docker.mk
 include ./mk-include/cc-end.mk
+
+# Custom docker targets
+.PHONY: show-docker-all
+show-docker-all:
+	@echo
+	@echo ========================
+	@echo "Docker info for ce-kafka:"
+	@make VERSION=$(VERSION) show-docker
+	@echo
+	@echo ========================
+	@echo "Docker info for soak_cluster"
+	@make VERSION=$(VERSION) -C cc-services/soak_cluster show-docker
+	@echo
+	@echo ========================
+	@echo "Docker info for trogdor"
+	@make VERSION=$(VERSION) -C cc-services/trogdor show-docker
+
+.PHONY: build-docker-override
+build-docker-override: .netrc .ssh docker-pull-base $(DOCKER_BUILD_PRE) build-docker-ce-kafka build-docker-cc-services show-docker-all
+
+.PHONY: build-docker-ce-kafka
+build-docker-ce-kafka:
+	docker build --no-cache --build-arg version=$(IMAGE_VERSION) -t $(BUILD_TAG) .
+	rm -rf .netrc .ssh
+
+.PHONY: build-docker-cc-services
+build-docker-cc-services:
+	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/soak_cluster build-docker
+	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/trogdor build-docker
+
+.PHONY: push-docker-latest
+push-docker-override: push-docker-latest push-docker-version push-docker-cc-services
+
+.PHONY: push-docker-cc-services
+push-docker-cc-services:
+	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/soak_cluster push-docker
+	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/trogdor push-docker

cc-services/soak_cluster/Makefile
@@ -3,24 +3,41 @@ IMAGE_NAME := cc-$(SERVICE_NAME)
 CHART_NAME := cc-$(SERVICE_NAME)
 CODECOV := true
 MAIN_GO := ./main.go
-
 GO_OUTDIR ?= bin
 GO_TEST_TARGET = test-go
 GO_TEST_ARGS = -race -v -cover -p=1
 GO_CODECOV_TEST_ARGS = -race -v -cover -p=1
 
+MASTER_BRANCH := ce-trunk
+
+SOAK_TEST_NAMESPACE ?= soak-tests
+
+DOCKER_BUILD_PRE = sync-mkinclude
+.PHONY: sync-mkinclude
+sync-mkinclude:
+	rsync -avz --delete-after ../../mk-include/ ./.mk-include.local-context-copy
 
 include ./mk-include/cc-begin.mk
+ifeq ($(VERSION),)
 include ./mk-include/cc-semver.mk
+endif
 include ./mk-include/cc-go.mk
 include ./mk-include/cc-docker.mk
+include ./mk-include/cc-cpd.mk
+include ./mk-include/cc-helm.mk
 include ./mk-include/cc-end.mk
-
-sync-mkinclude:
-	rsync -avz --delete-after ../../mk-include/ ./mk-include/
-
-local-build-docker: sync-mkinclude build-docker
 
 $(GO_OUTDIR)/soak-clients:
 	go build -o $@ -ldflags $(GO_LDFLAGS) soak_cluster/main.go
 
+.PHONY: helm-deploy-soak
+## Deploy helm to current kube context with values set to local.yaml on the soak-tests namespace
+helm-deploy-soak:
+	helm upgrade \
+	--install $(CHART_NAME)-dev \
+	charts/$(CHART_NAME) \
+	--namespace $(SOAK_TEST_NAMESPACE) \
+	--set namespace=$(SOAK_TEST_NAMESPACE) \
+	--debug \
+	-f charts/values/local.yaml \
+	--set image.tag=$(CHART_VERSION) $(HELM_ARGS)

cc-services/trogdor/Makefile
@@ -1,10 +1,30 @@
-IMAGE_NAME := ce-trogdor
-BASE_IMAGE := confluent-docker.jfrog.io/confluentinc/ce-kafka
-BASE_VERSION := latest
+SERVICE_NAME := trogdor
+IMAGE_NAME := cc-$(SERVICE_NAME)
+CHART_NAME := cc-$(SERVICE_NAME)-service
 RELEASE_BRANCH := ce-trunk
 DEFAULT_BUMP := minor
+
+BASE_IMAGE ?= confluent-docker.jfrog.io/confluentinc/ce-kafka
+# Use ce-truck clean version as docker base verson unless specified
+BASE_VERSION ?= $(shell git rev-parse --is-inside-work-tree > /dev/null && git describe --tags --always ce-trunk)
+
+SOAK_TEST_NAMESPACE ?= soak-tests
 
 include ./mk-include/cc-begin.mk
 include ./mk-include/cc-semver.mk
 include ./mk-include/cc-docker.mk
+include ./mk-include/cc-cpd.mk
+include ./mk-include/cc-helm.mk
 include ./mk-include/cc-end.mk
+
+.PHONY: helm-deploy-soak
+## Deploy helm to current kube context with values set to local.yaml on the soak-tests namespace
+helm-deploy-soak:
+	helm upgrade \
+	--install $(CHART_NAME)-dev \
+	charts/$(CHART_NAME) \
+	--namespace $(SOAK_TEST_NAMESPACE) \
+	--set namespace=$(SOAK_TEST_NAMESPACE) \
+	--debug \
+	-f charts/values/local.yaml \
+	--set image.tag=$(CHART_VERSION) $(HELM_ARGS)

cc-services/soak_cluster/.gitignore
@@ -1,0 +1,3 @@
+.mk-include.local-context-copy
+charts/package
+.gomodcache

cc-services/soak_cluster/Dockerfile
@@ -8,10 +8,11 @@ COPY .netrc ./
 RUN ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
 
 WORKDIR /go/src/github.com/confluentinc/ce-kafka/cc-services/soak_cluster
-COPY mk-include ./mk-include
-COPY Gopkg.toml Gopkg.lock Makefile ./
 COPY . .
-RUN make deps DEP_ARGS=-vendor-only
+# Replace mk-include link with the local context copy
+RUN rm ./mk-include
+COPY .mk-include.local-context-copy ./mk-include
+RUN make deps DEP_ARGS=-vendor-only VERSION=${version}
 
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 make build-go GO_OUTDIR= VERSION=${version}
 

commit 94e3338571f0c5c47d1ca5b746a8149bb66206aa

CONFLUENT: Do not build cc-services  via the "make build" target (#170)
There is a problem in our current setup that prevents the Docker images from cc-services to build in CI. This is a regression from commit f6e5c51c2522e061bc523cddbd2795c0bdd9129c (https://github.com/confluentinc/ce-kafka/commit/f6e5c51c2522e061bc523cddbd2795c0bdd9129c)

We wanted to automatically build the cc-services in CI via the "make build" command. Due to our makefiles having conditional logic depending on whether they are running in CI and whether they are running the main branch (ce-trunk), we did not catch this issue during testing.
We are removing the automatic built of the cc-services in the
main makefile by removing the override on the build docker targets.

Reviewers: Stanislav <stanislav@confluent.io>

Makefile
@@ -5,10 +5,6 @@ MASTER_BRANCH := ce-trunk
 KAFKA_VERSION := $(shell awk 'sub(/.*version=/,""){print $1}' ./gradle.properties)
 VERSION_POST := -$(KAFKA_VERSION)
 
-# Override docker default targets
-BUILD_DOCKER_OVERRIDE := build-docker-override
-PUSH_DOCKER_OVERRIDE := push-docker-override
-
 include ./mk-include/cc-begin.mk
 include ./mk-include/cc-semver.mk
 include ./mk-include/cc-docker.mk
@@ -30,21 +26,10 @@ show-docker-all:
 	@echo "Docker info for trogdor"
 	@make VERSION=$(VERSION) -C cc-services/trogdor show-docker
 
-.PHONY: build-docker-override
-build-docker-override: .netrc .ssh docker-pull-base $(DOCKER_BUILD_PRE) build-docker-ce-kafka build-docker-cc-services show-docker-all
-
-.PHONY: build-docker-ce-kafka
-build-docker-ce-kafka:
-	docker build --no-cache --build-arg version=$(IMAGE_VERSION) -t $(BUILD_TAG) .
-	rm -rf .netrc .ssh
-
 .PHONY: build-docker-cc-services
 build-docker-cc-services:
 	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/soak_cluster build-docker
 	make VERSION=$(VERSION) BASE_IMAGE=$(IMAGE_REPO)/$(IMAGE_NAME) BASE_VERSION=$(IMAGE_VERSION) -C cc-services/trogdor build-docker
-
-.PHONY: push-docker-latest
-push-docker-override: push-docker-latest push-docker-version push-docker-cc-services
 
 .PHONY: push-docker-cc-services
 push-docker-cc-services: