4f63ae62181a0e871fbdf7af0547ff18849d8752
Lucas Bradstreet <lucas@confluent.io>
2019-02-25 10:44:55 -0800

    Fix typo in ApacheDirectoryEndToEndAuthorizationTest parameters.

--- ce-auth-providers/src/test/java/io/confluent/security/test/integration/ldap/ApacheDirectoryEndToEndAuthorizationTest.java
+++ ce-auth-providers/src/test/java/io/confluent/security/test/integration/ldap/ApacheDirectoryEndToEndAuthorizationTest.java
@@ -140,7 +140,7 @@ public class ApacheDirectoryEndToEndAuthorizationTest extends AbstractEndToEndAu
     return User.gssapiUser(name, principal + "@EXAMPLE.COM", keytabFile, serviceName);
   }
 
-  @Parameterized.Parameters(name = "saslMechanisnm={0}, secProtocol={1}, secAuth={2},"
+  @Parameterized.Parameters(name = "saslMechanism={0}, secProtocol={1}, secAuth={2},"
           + "secUser={3}, refreshInterval={4}")
   public static Collection<Object[]> data() {
     List<Object[]> values = new ArrayList<>();
