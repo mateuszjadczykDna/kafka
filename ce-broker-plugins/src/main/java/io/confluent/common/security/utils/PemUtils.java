e28b855fb23b2b3dcde5c29ef5612704b7d13c39
Ryan P <ryan@confluent.io>
2019-04-10 11:38:09 -0400

    CONFLUENT: Add KeyPair PEM reader/writer to PemUtils (#259)

--- ce-broker-plugins/src/main/java/io/confluent/common/security/utils/PemUtils.java
+++ ce-broker-plugins/src/main/java/io/confluent/common/security/utils/PemUtils.java
@@ -9,6 +9,7 @@ import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.util.io.pem.PemWriter;
+import org.bouncycastle.openssl.PEMKeyPair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,6 +18,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
+import java.security.KeyPair;
 import java.security.PublicKey;
 
 public class PemUtils {
@@ -34,6 +36,20 @@ public class PemUtils {
   public static void writePublicKey(OutputStream out, PublicKey key) throws IOException {
     try (PemWriter pemWriter = new PemWriter(new OutputStreamWriter(out, "utf8"))) {
       pemWriter.writeObject(new JcaMiscPEMGenerator(key));
+    }
+  }
+
+  public static KeyPair loadKeyPair(InputStream inputStream) throws IOException {
+    try (InputStreamReader reader = new InputStreamReader(inputStream, US_ASCII))  {
+      PEMParser pemParser = new PEMParser(new BufferedReader(reader));
+      PEMKeyPair pemKeyPair = (PEMKeyPair) pemParser.readObject();
+      return new JcaPEMKeyConverter().getKeyPair(pemKeyPair);
+    }
+  }
+
+  public static void writeKeyPair(OutputStream out, KeyPair keyPair) throws IOException {
+    try (PemWriter pemWriter = new PemWriter(new OutputStreamWriter(out, "utf8"))) {
+      pemWriter.writeObject(new JcaMiscPEMGenerator(keyPair));
     }
   }
 }
--- ce-broker-plugins/src/test/java/io/confluent/common/security/utils/PemUtilsTest.java
+++ ce-broker-plugins/src/test/java/io/confluent/common/security/utils/PemUtilsTest.java
@@ -4,13 +4,15 @@
 
 package io.confluent.common.security.utils;
 
-import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
+
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -36,5 +38,34 @@ public class PemUtilsTest {
     String outputString = out.toString("utf8");
     assertTrue(outputString.startsWith("-----BEGIN PUBLIC KEY-----\n"));
     assertTrue(outputString.endsWith("-----END PUBLIC KEY-----\n"));
+  }
+
+  @Test
+  public void testReadKeyPair() throws Exception {
+    PipedInputStream in = new PipedInputStream();
+    PipedOutputStream out = new PipedOutputStream(in);
+
+    final KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+    generator.initialize(2048);
+    KeyPair keyPair = generator.generateKeyPair();
+
+    new Thread(
+            new Runnable() {
+              @Override
+              public void run() {
+                try {
+                  PemUtils.writeKeyPair(out, keyPair);
+                  out.close();
+                } catch (Exception e) {
+                  //noop
+                }
+              }
+            }
+    ).start();
+
+    KeyPair loadedKeyPair = PemUtils.loadKeyPair(in);
+    in.close();
+    assertTrue(keyPair.getPublic().equals(loadedKeyPair.getPublic()));
+    assertTrue(keyPair.getPrivate().equals(loadedKeyPair.getPrivate()));
   }
 }
